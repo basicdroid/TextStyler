b95109cc2004cea0b38cde4372014fb59701e464
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-05-14 10:00:04 +0800

    Demonstrating the usage of TextWatcher.

--- app/src/main/java/com/training/android/textstyler/MainActivity.java
+++ app/src/main/java/com/training/android/textstyler/MainActivity.java
@@ -1,13 +1,41 @@
 package com.training.android.textstyler;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtInput;
+    private TextView mTvStyledText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEtInput = (EditText) findViewById(R.id.etText);
+        mTvStyledText = (TextView) findViewById(R.id.tvStyledText);
+
+        // Set appropriate listeners or watchers..
+        mEtInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing..
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing..
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mTvStyledText.setText(s);
+            }
+        });
     }
 }
